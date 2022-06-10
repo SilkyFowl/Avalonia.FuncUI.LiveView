50b11937e088bc1d3578bda7633a1508df3a0128
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2022-06-10 09:12:33 +0900

    update settings.

--- build.fsx
+++ build.fsx
@@ -23,18 +23,64 @@ open Fake.Core.TargetOperators
 let srcPath = "./src"
 let outputPath = "./dist"
 
-let analyzerProjName = "Avalonia.FuncUI.LiveView.Analyzer"
-let analyzerPath = srcPath @@ analyzerProjName
-let locallocalanalyzerPath = "./localanalyzers"
-
 // properties
-let projectDescription = ["Live fs/fsx previewer for Avalonia.FuncUI."]
+let projectDescription = [ "Live fs/fsx previewer for Avalonia.FuncUI." ]
 let gitUserName = "SilkyFowl"
-let authors = [gitUserName]
+let authors = [ gitUserName ]
 let projectUrl = $"https://github.com/{gitUserName}/Avalonia.FuncUI.LiveView"
 
 let release = ReleaseNotes.load "RELEASE_NOTES.md"
 
+
+module AnalyzerProjInfo =
+    let name = "Avalonia.FuncUI.LiveView.Analyzer"
+    let path = srcPath @@ name
+    let localanalyzerPath = "./localanalyzers"
+
+module LiveViewProjInfo =
+    let name = "Avalonia.FuncUI.LiveView"
+    let path = srcPath @@ name
+
+let toTemplateFilePath projectPath = Some(projectPath @@ "paket.template")
+
+let idWithGitUserName projectName = $"{gitUserName}.{projectName}"
+
+module Paket =
+    open PaketTemplate
+
+    let private baseParams =
+        { DefaultPaketTemplateParams with
+            TemplateType = Project
+            Version = Some release.NugetVersion
+            ReleaseNotes = release.Notes
+            Description = projectDescription
+            Authors = authors
+            ProjectUrl = Some projectUrl }
+
+    let settings =
+        [ {| path = AnalyzerProjInfo.path
+             templateParams =
+              { baseParams with
+                  TemplateFilePath = toTemplateFilePath AnalyzerProjInfo.path
+                  Id = (idWithGitUserName >> Some) AnalyzerProjInfo.name
+                  Files = [ Include("bin" </> "Release" </> "net6.0" </> "publish", "lib" </> "net6.0") ] } |}
+          {| path = LiveViewProjInfo.path
+             templateParams =
+              { baseParams with
+                  TemplateFilePath = toTemplateFilePath LiveViewProjInfo.path
+                  Id = (idWithGitUserName >> Some) LiveViewProjInfo.name } |} ]
+
+
+module PaketTemplate =
+    let createWithLicenseExpression licenseExpression (paketTemplateParams: PaketTemplate.PaketTemplateParams) =
+        match paketTemplateParams.TemplateFilePath with
+        | None -> invalidArg "paketTemplateParams" "must set TemplateFilePath."
+        | Some templateFilePath ->
+            PaketTemplate.create (fun _ -> paketTemplateParams)
+
+            File.append templateFilePath [ $"licenseExpression {licenseExpression}" ]
+
+    let createWithLicenseMIT = createWithLicenseExpression "MIT"
 
 Target.initEnvironment ()
 
@@ -50,44 +96,38 @@ Target.create "Clean" (fun _ ->
 Target.create "Build" (fun _ -> DotNet.build id "./Avalonia.FuncUI.LiveView.sln")
 
 Target.create "PackAnalyzer" (fun _ ->
+    for setting in Paket.settings do
 
-    let templateFilePath = analyzerPath @@ "paket.template"
+        setting.templateParams
+        |> PaketTemplate.createWithLicenseMIT
 
-    PaketTemplate.create (fun p ->
-        { p with
-            TemplateType = PaketTemplate.Project
-            TemplateFilePath = Some templateFilePath
-            Id = Some analyzerProjName
-            Version = Some release.NugetVersion
-            ReleaseNotes = release.Notes
-            Description = projectDescription
-            Authors = authors
-            ProjectUrl = Some projectUrl
-            Files = [ PaketTemplate.Include("bin" </> "Release" </> "net6.0" </> "publish", "lib" </> "net6.0") ] })
-    
-    ["licenseExpression MIT"]
-    |> File.append templateFilePath
+        setting.path
+        |> DotNet.publish (fun opts ->
+            { opts with
+                Configuration = DotNet.Release
+                SelfContained = Some false
+                Framework = Some "net6.0" })
 
-    analyzerPath
-    |> DotNet.publish (fun opts ->
-        { opts with
-            Configuration = DotNet.Release
-            Framework = Some "net6.0" })
-
-    Paket.pack (fun p ->
-        { p with
-            ToolType = ToolType.CreateLocalTool()
-            OutputPath = outputPath }))
-
+        let isAvalyzer = setting.path = AnalyzerProjInfo.path
+        Paket.pack (fun p ->
+            { p with
+                ToolType = ToolType.CreateLocalTool()
+                TemplateFile = Option.toObj setting.templateParams.TemplateFilePath
+                OutputPath = outputPath
+                MinimumFromLockFile = not isAvalyzer
+                IncludeReferencedProjects = not isAvalyzer
+                }))
 
 Target.create "ClearLocalAnalyzer" (fun _ ->
-    !!outputPath ++ locallocalanalyzerPath
+    !!outputPath ++ AnalyzerProjInfo.localanalyzerPath
     |> Shell.cleanDirs)
 
 Target.create "SetLocalAnalyzer" (fun _ ->
+    let analyzerId = idWithGitUserName AnalyzerProjInfo.name
+
     outputPath
-    |> Directory.findFirstMatchingFile $"{analyzerProjName}.*"
-    |> Zip.unzip (locallocalanalyzerPath </> analyzerProjName))
+    |> Directory.findFirstMatchingFile $"{analyzerId}.*"
+    |> Zip.unzip (AnalyzerProjInfo.localanalyzerPath </> analyzerId))
 
 Target.create "Default" ignore
 
